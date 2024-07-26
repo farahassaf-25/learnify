dc58d4905ff6e270b1cdbf997270c3fffebd015d
farahassaf-25 <assaffarah25@gmail.com>
2024-07-26 16:58:10 +0300

    add some security(sanitize data, xss protection, limit req, hpp, enable cors)

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -1,7 +1,17 @@
-//@decsc logger middleware: log req to console
+const colors = require('colors');
+
 const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    const mthdsColors = {
+        GET: 'green',
+        POST: 'yellow', 
+        PUT: 'blue', 
+        DELETE: 'magenta',
+    };
+
+    const color = mthdsColors[req.method] || white;
+
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`[color]);
     next();
-}
+};
 
 module.exports = logger;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const colors = require('colors');
-const morgan = require('morgan');
-const errorHandler = require('./middleware/error');
+// const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const sanitizeData = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xssClean = require('xss-clean');
+const limitRequest = require('express-rate-limit');
+const cors = require('cors');
+const hpp = require('hpp');
 
+const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/error');
 // Route files
 const courses = require('./routes/courses');
 const lectures = require('./routes/lectures');
@@ -21,10 +28,35 @@ app.use(express.json());
 //cookie parser
 app.use(cookieParser());
 
+//logger middleware
+app.use(logger);
+
+//sanitize data
+app.use(sanitizeData());
+
+//set security headers
+app.use(helmet());
+
+//prevent xss attacks
+app.use(xssClean());
+
+//enable cors
+app.use(cors()); 
+
+//limit requests from same API
+const limit = limitRequest({
+    windowMs: 10 * 60 * 1000, //10 mins
+    max: 20, //20 times limit
+});
+app.use(limit);
+
+//prevent http param pollution
+app.use(hpp());
+
 // Dev logging middleware
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-}
+// if (process.env.NODE_ENV === 'development') {
+//     app.use(morgan('dev'));
+// }
 
 // Mount routes
 app.use('/learnify/courses', courses);
