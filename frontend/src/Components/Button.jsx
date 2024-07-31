bda2863ce3c264b449d1cf944fd1998bed009c5c
farahassaf-25 <assaffarah25@gmail.com>
2024-07-31 23:41:30 +0300

    update button

--- frontend/src/Components/Button.jsx
+++ frontend/src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Button = ({ children, color, to, onClick }) => {
-  const baseClasses = "font-bold py-2 px-4 rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2";
+  const baseClasses = "font-bold py-4 px-4 rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2";
   const colorClasses = {
     primary: "bg-primary hover:bg-primary-light text-white focus:ring-primary",
     secondary: "bg-secondary hover:bg-secondary-light text-white focus:ring-secondary",
