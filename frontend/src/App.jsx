e7399942b811a62d5053c1595b05bc4c3384f8d7
farahassaf-25 <assaffarah25@gmail.com>
2024-08-15 21:47:47 +0300

    add ToastContainer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './Components/Navbar';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
@@ -30,9 +32,8 @@ const App = () => {
         <Route path='/me' element={<UserProfilePage />} />
         <Route path="/confirmation" element={<ConfirmationPage />} />
       </Route>
-
-        
       </Routes>
+      <ToastContainer />
     </MainLayout>
   );
 };
